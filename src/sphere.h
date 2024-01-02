1c9ce77a96f4a8947df76be483df2787919ecf6f
Fumitaka Tokumitsu <3142849+toku345@users.noreply.github.com>
2024-01-02 16:34:31 +0900

    6.7. Common Constants and Utility Functions

--- src/sphere.h
+++ src/sphere.h
@@ -32,7 +32,7 @@ public:
 
         rec.t = root;
         rec.p = r.at(rec.t);
-        vec3 = outward_normal = (rec.p - center) / radius;
+        vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
 
         return true;
