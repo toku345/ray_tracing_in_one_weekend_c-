0121c1b95c619650471529010252fc605e495d02
Fumitaka Tokumitsu <3142849+toku345@users.noreply.github.com>
2024-01-07 01:04:39 +0900

    9.2. Limiting the Number of Child Rays

--- src/camera.h
+++ src/camera.h
@@ -14,6 +14,7 @@ public:
     double aspect_ratio = 1.0;  // Ratio of image width over height
     int image_width = 100;      // Rendered image width in pixel count
     int samples_per_pixel = 10; // Count of random
+    int max_depth = 10;         // Maximum number of ray bounces into space
 
     void render(const hittable &world)
     {
@@ -32,7 +33,7 @@ public:
                 for (int sample = 0; sample < samples_per_pixel; ++sample)
                 {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(std::cout, pixel_color, samples_per_pixel);
             }
@@ -95,14 +96,18 @@ private:
         return (px * pixel_delta_u) + (py * pixel_delta_v);
     }
 
-    color ray_color(const ray &r, const hittable &world)
+    color ray_color(const ray &r, int depth, const hittable &world)
     {
         hit_record rec;
+
+        // If we've exceeded the ray bounce limit, no more light is gathered.
+        if (depth <= 0)
+            return color(0, 0, 0);
 
         if (world.hit(r, interval(0, infinity), rec))
         {
             vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
