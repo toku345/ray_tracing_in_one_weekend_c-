ce3a6a4aa76a941152f57bc40d7c76ea76608815
Fumitaka Tokumitsu <3142849+toku345@users.noreply.github.com>
2024-01-07 16:46:07 +0900

    10.4. Mirrored Light Reflection

--- src/material.h
+++ src/material.h
@@ -37,4 +37,22 @@ private:
     color albedo;
 };
 
+class metal : public material
+{
+public:
+    metal(const color &a) : albedo(a) {}
+
+    bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered)
+        const override
+    {
+        vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+        scattered = ray(rec.p, reflected);
+        attenuation = albedo;
+        return true;
+    }
+
+private:
+    color albedo;
+};
+
 #endif
